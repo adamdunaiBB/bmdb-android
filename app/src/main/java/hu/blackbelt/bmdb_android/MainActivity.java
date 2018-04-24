408c37dd0aed599c2076488c4b761bd8adf78240
Adam Dunai <adam.dunai@blackbelt.hu>
2018-04-24 15:03:36 +0200

    Set up default launcher activity

--- app/src/main/java/hu/blackbelt/bmdb_android/MainActivity.java
+++ app/src/main/java/hu/blackbelt/bmdb_android/MainActivity.java
@@ -3,11 +3,13 @@ package hu.blackbelt.bmdb_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
