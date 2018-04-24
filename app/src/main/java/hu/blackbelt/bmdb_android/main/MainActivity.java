a76d02ffad418ec5e8784a6217083858e803295b
Adam Dunai <adam.dunai@blackbelt.hu>
2018-04-24 16:26:24 +0200

    Refactor MainActivity

--- app/src/main/java/hu/blackbelt/bmdb_android/main/MainActivity.java
+++ app/src/main/java/hu/blackbelt/bmdb_android/main/MainActivity.java
@@ -1,9 +1,11 @@
-package hu.blackbelt.bmdb_android;
+package hu.blackbelt.bmdb_android.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.androidannotations.annotations.EActivity;
+
+import hu.blackbelt.bmdb_android.R;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
